f86164c0b36b5e9b387fe58e022cd181ed20eed6
DavidmB <david.m.bedford1@gmail.com>
2019-05-21 14:04:57 -0400

    api route js almost complete, moving to fix materialize

---
+++ app/data/friends.js
@@ -0,0 +1,48 @@
+//Friend Finder API
+
+let friendsArray = {
+  "name":"Yarg",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ],
+  "name":"Bjorn",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      2,
+      2,
+      4,
+      3,
+      2,
+      5,
+      4,
+      3,
+      2,
+      1
+      ],
+  "name":"Hagrid",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      3,
+      2,
+      3,
+      4,
+      5,
+      2,
+      2,
+      5,
+      3,
+      1
+    ]
+};
+
+module.exports = friendsArray;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,13 +1,4 @@
-// ===============================================================================
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
-// ===============================================================================
-var path = require("path");
-
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
+const path = require("path");
 
 module.exports = function(app) {
   // HTML GET Requests
@@ -15,12 +6,12 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
-  app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/tables.html"));
+  app.get("/home", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
-  app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/reserve.html"));
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
   // If no matching route is found default to home
